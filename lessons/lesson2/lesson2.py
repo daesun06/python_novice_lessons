e2c42e01406756cbc0d01f0becb1e436632923b8
Daesun <deson_m@icloud.com>
2020-12-14 22:11:17 +0600

    HW 2

--- lessons/lesson2/lesson2.py
+++ lessons/lesson2/lesson2.py
@@ -1,27 +1,69 @@
 def section_5_solution_1():
     # Write your solution here
 
+money=2000
+if money > 1000:
+    print("I'm rich!!")
+else:
+    print("I'm not reach!!")
+        print("But I might be later...")
+
+#I cannot understand where is the problem.
+
 def section_5_solution_2():
     # Write your solution here
 
-def section_5_solution_1():
+twinkies=60
+if twinkies > 100:
+    print("That's to many!")
+else:
+    print("That's to few!")
+
+
+def section_5_solution_3():
     # Write your solution here
 
-def section_5_solution_1():
+money=2500
+if money > 1000:
+    print("That's to many!")
+else:
+    print("That's to few!")
+
+
+def section_5_solution_4():
     # Write your solution here
+
+ninjas=16
+if ninjas < 30:
+    print("I can take them!")
+elif:
+    print("It will be hard, but I can take them.")
 
 
 def section_6_solution_1():
     # Write your solution here
+
+I think it will break.
 
 def section_6_solution_2():
     # Write your solution here
 
-def section_6_solution_1():
+I don't understand what to do after 
+
+for number in range(0, 10)
+
+def section_6_solution_3():
     # Write your solution here
 
-def section_6_solution_1():
+ingrediants=['salad', 'tomato', 'cheese', 'meat', 'bread']
+for d in ingrediants:
+    print(d)
+
+def section_6_solution_4():
     # Write your solution here
+
+I didnt understand the puzzle.
+
     # question 1 : 
 
 
