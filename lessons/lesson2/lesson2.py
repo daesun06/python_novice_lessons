22a5cce101e682e6693c17bfde0fac9a4b279cc7
Rahimzhan Orumbayev <rahim@Rahimzhans-iMac.local>
2020-12-15 18:44:55 +0600

    HW 2 partial verification

--- lessons/lesson2/lesson2.py
+++ lessons/lesson2/lesson2.py
@@ -1,70 +1,85 @@
 def section_5_solution_1():
     # Write your solution here
 
-money=2000
-if money > 1000:
-    print("I'm rich!!")
-else:
-    print("I'm not reach!!")
+    money=2000
+    if money > 1000:
+        print("I'm rich!!")
+    else:
+        print("I'm not reach!!")
         print("But I might be later...")
 
-#I cannot understand where is the problem.
+    #I cannot understand where is the problem.
+
+section_5_solution_1()
 
 def section_5_solution_2():
     # Write your solution here
 
-twinkies=60
-if twinkies > 100:
-    print("That's to many!")
-else:
-    print("That's to few!")
+    # 0 1 = 0 + 1 = 1
+    # 0 1 = 0 * 1 = 0
+    # errors, using AND and OR conditionals, using elif instead of else
+    twinkies=60
+    if twinkies < 100:
+        print("That's to few!")
+    elif twinkies > 500:
+        print("That's to many!")
+
+section_5_solution_2()
+
+def section_5_solution_3(money: int):
+    # Write your solution here
+    # Deson's code
+    # money=2500
+    # if money > 1000:
+    #     print("That's to many!")
+    # else:
+    #     print("That's to few!")
+
+    if money >= 100 and money <= 500 or money >= 1000 and money <= 5000:
+        return True
+
+    return False 
+
+print(section_5_solution_3(99))
+print(section_5_solution_3(1250))
+print(section_5_solution_3(5001))
 
 
-def section_5_solution_3():
+# def section_5_solution_4():
     # Write your solution here
 
-money=2500
-if money > 1000:
-    print("That's to many!")
-else:
-    print("That's to few!")
+    # ninjas=16
+    # if ninjas < 30:
+    #     print("I can take them!")
+    # elif:
+    #     print("It will be hard, but I can take them.")
 
 
-def section_5_solution_4():
-    # Write your solution here
+# def section_6_solution_1():
+#     # Write your solution here
 
-ninjas=16
-if ninjas < 30:
-    print("I can take them!")
-elif:
-    print("It will be hard, but I can take them.")
+#     #I think it will break.
 
+# def section_6_solution_2():
+#     # Write your solution here
 
-def section_6_solution_1():
-    # Write your solution here
+#     #I don't understand what to do after 
 
-I think it will break.
+#     for number in range(0, 10)
 
-def section_6_solution_2():
-    # Write your solution here
+# def section_6_solution_3():
+#     # Write your solution here
 
-I don't understand what to do after 
+#     ingrediants=['salad', 'tomato', 'cheese', 'meat', 'bread']
+#     for d in ingrediants:
+#         print(d)
 
-for number in range(0, 10)
+# def section_6_solution_4():
+#     # Write your solution here
 
-def section_6_solution_3():
-    # Write your solution here
+#     I didnt understand the puzzle.
 
-ingrediants=['salad', 'tomato', 'cheese', 'meat', 'bread']
-for d in ingrediants:
-    print(d)
-
-def section_6_solution_4():
-    # Write your solution here
-
-I didnt understand the puzzle.
-
-    # question 1 : 
+#     # question 1 : 
 
 
-section_6_solution_1()
+# section_6_solution_1()
