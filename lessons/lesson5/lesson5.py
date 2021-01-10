c50f320e175d1a5768810e5f8d972a66ac2e6ce5
Daesun Magay <deson_m@icloud.com>
2021-01-10 16:50:56 +0600

    HW 5

---
+++ lessons/lesson5/lesson5.py
@@ -0,0 +1,46 @@
+#class Human:
+    #def sleep():
+        #pass
+    #def eat():
+        #pass
+    #def breathe():
+        #pass
+#class Nari(Human):
+    #def annoy():
+        #pass
+
+
+#human = Human()
+#nari = Nari()
+
+
+## HW 4
+
+#1
+
+class Giraffes(self)
+    def left_Foot_Foward(self):
+        print('left foot foward')
+    def left_Foot_Back(self):
+        print('left foot back')
+    def right_Foot_Foward(self):
+        print('right foot foward')
+    def right_Foot_Back(self):
+        print('right foot back')
+    def left_Foot_Back(self):
+        print('left foot back')
+    def right_Foot_Back(self):
+        print('right foot back')
+    def right_Foot_Foward(self):
+        print('right foot foward')
+    def left_Foot_Foward(self):
+        print('left foot foward')
+class Reginald(Giraffes())
+    def dance:
+
+
+
+
+
+reginald = Giraffes()
+reginald.dance
