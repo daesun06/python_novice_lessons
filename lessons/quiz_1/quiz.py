e1762353a41fa0f6a606cd589bedbc991fe0d95d
Rahimzhan Orumbayev <rahim@Rahimzhans-iMac.local>
2021-01-19 19:43:55 +0600

    wip sol2

--- lessons/quiz_1/quiz.py
+++ lessons/quiz_1/quiz.py
@@ -3,4 +3,6 @@ def solution_1():
     if age == 10 or age == 11 or age == 12:
         print("You're still a kiddo!")
     
-        
+def solution_2(first_name, last_name):
+    print(f'{first_name} {last_name} hello !')
+    
