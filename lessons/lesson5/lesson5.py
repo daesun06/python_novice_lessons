04bbda4508af51130ff6b5466a6cfcab4cd28e45
Daesun Magay <deson_m@icloud.com>
2021-01-24 21:20:47 +0600

    Quiz_2 Completion

--- lessons/lesson5/lesson5.py
+++ lessons/lesson5/lesson5.py
@@ -16,31 +16,81 @@
 
 ## HW 4
 
-#1
+###1
 
-class Giraffes(self)
-    def left_Foot_Foward(self):
-        print('left foot foward')
+class Animals:
+    sound = "roar"
+
+    def breath(self):
+        print('breathing')
+    def walk(self):
+        print('walking')
+    def eat(self):
+        print('eating')
+
+class Mammals(Animals):
+    def feed_kids_with_milk(self):
+        print('feeding')
+
+class Giraffes(Mammals):
+    def left_Foot_Forward(self):
+        print('left foot forward')
     def left_Foot_Back(self):
         print('left foot back')
-    def right_Foot_Foward(self):
-        print('right foot foward')
+    def right_Foot_Forward(self):
+        print('right foot forward')
     def right_Foot_Back(self):
         print('right foot back')
-    def left_Foot_Back(self):
-        print('left foot back')
-    def right_Foot_Back(self):
-        print('right foot back')
-    def right_Foot_Foward(self):
-        print('right foot foward')
-    def left_Foot_Foward(self):
-        print('left foot foward')
-class Reginald(Giraffes())
-    def dance:
-
-
-
+    def dance(self):
+        self.left_Foot_Forward()
+        self.left_Foot_Back()
+        self.right_Foot_Forward()
+        self.right_Foot_Back()
+        self.left_Foot_Back()
+        self.right_Foot_Back()
+        self.right_Foot_Forward()
+        self.left_Foot_Forward()
 
 
 reginald = Giraffes()
-reginald.dance
+reginald.dance()
+
+###2
+import time 
+import turtle 
+nari = turtle.Pen()
+deson = turtle.Pen()
+fred = turtle.Pen()
+frank = turtle.Pen()
+
+nari.forward(100)
+nari.left(90)
+nari.forward(50)
+nari.right(90)
+nari.forward(50)
+
+deson.forward(110)
+deson.left(90)
+deson.forward(25)
+deson.right(90)
+deson.forward(25)
+
+
+fred.forward(100)
+fred.right(90)
+fred.forward(50)
+fred.left(90)
+fred.forward(50)
+
+frank.forward(110)
+frank.right(90)
+frank.forward(25)
+frank.left(90)
+frank.forward(25)
+
+time.sleep(60)
+
+
+
+
+
--- lessons/quiz_2/tasks.py
+++ lessons/quiz_2/tasks.py
@@ -5,7 +5,14 @@
 # False если число не четное
 # Программа должна запускаться без ошибок!
 # За правильное выполнение выдается 6 баллов!
+ 
+def even_or_odd(number:int):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
 
+print(even_or_odd(7)) # 5 points 
 
 
 # Задание 2. Создайте программу которая выполняет следующее
@@ -15,7 +22,16 @@
 # Иначе для всех остальных значений функция должна напечатать "Incorrect value given!"
 # Программа должна запускаться без ошибок!
 # За правильное выполнение выдается 6 баллов!
+ 
+def male_or_female(name):
+    if name == "male":
+        print("Hello i am a boy!")
+    elif name == "female":
+        print("Hello i am a girl!")
+    else:
+        print("Incorrect value given!")
 
+male_or_female("deson") # 6 points 
 
 # Задание 3. Создайте класс Clock которая содержит функцию get_time.
 # Функция get_time должна использовать функцию asctime из модуля time и печатать текущее время в терминал.
@@ -24,3 +40,13 @@
 # Вместо времени функция get_time должна печатать 'Hello {name} here is current time {time}'.
 # Где переменные name и time представляют значение name переданное в конструктор и time из
 # функции asctime из модуля time.
+
+import time
+class Clock: # ошибка
+    def get_time(self):
+        print(time.asctime()) 
+# Hint 1 - minus 1 point
+
+clock = Clock()
+print(clock.get_time())
+# 14 out of 18 
