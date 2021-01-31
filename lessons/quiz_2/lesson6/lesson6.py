b56c8d1612bb5b1556c8047ed58d7c35efb64b0b
Daesun Magay <deson_m@icloud.com>
2021-01-31 19:40:31 +0600

    HW6

---
+++ lessons/quiz_2/lesson6/lesson6.py
@@ -0,0 +1,20 @@
+#N1
+
+#I think that in the first one it will add 10 to 10, and then
+#in the second one it will return 0.
+
+a = abs(10) + abs(-10)
+print(a)
+b = abs(-10) + -10
+print(b)
+
+
+#N2
+
+шифр = ("этот если способ вы плохо это подходит читаете для что-то шифрования пошло важных не сообщений так")
+print(dir(шифр))
+
+#N3
+
+def save_file():
+    \Компьютер\Локальный диск (C:)\Пользователи\lol\save\
