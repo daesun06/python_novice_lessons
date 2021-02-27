a8976e1b4a36181e159b49e53b6f7854f55e3168
Rahimzhan Orumbayev <rahim@Rahimzhans-iMac.local>
2021-02-27 18:44:30 +0600

    finalizing tasks

--- lessons/lesson7/lesson7.py
+++ lessons/lesson7/lesson7.py
@@ -27,7 +27,10 @@ print(car1.wheels)
 
 import pickle
 my_fav_things = ('guitar', 'steak', 'programming', 'lessons')
-save_file = open('favorites.dat', 'wb')
+f = open('favorites.dat', 'wb')
+pickle.dump(my_fav_things, f)
+f.close()
+
 load_file = open('favorites.dat', 'rb')
 loaded_my_fav_things = pickle.load(load_file)
 load_file.close()
@@ -40,28 +43,57 @@ print(loaded_my_fav_things)
 
 import turtle
 t = turtle.Pen()
-t.forward(50)
-t.right(45)
-t.forward(35)
-t.right(45)
-t.forward(50)
-t.right(45)
-t.forward(35)
-t.right(45)
-t.forward(50)
-t.right(45)
-t.forward(35)
-t.right(45)
-t.forward(50)
-t.right(45)
-t.forward(35)
-t.right(45)
+
+def octagon(size: int):
+  for _ in range(1, 9):
+    t.forward(size)
+    t.right(45)
+
+# octagon(100)
 
 ##N2
 
 ###I don't understan what programm you need to make, to color the figure.
 
+
+import turtle
+t = turtle.Pen()
+
+def octagon_with_color(size: int, filled: bool):
+  if filled == True:
+    t.begin_fill()
+
+  for _ in range(1, 9):
+    t.forward(size)
+    t.right(45)
+  
+  if filled == True:
+    t.end_fill()
+
+# t.color(1, 0.85, 0)
+# octagon_with_color(100, True)
+# t.color(0, 0, 0)
+# octagon_with_color(100, False)
+
+print("Help")
+
 ##N3
 
-def draw_star(size, points):
-    
+t = turtle.Pen()
+
+def draw_star(size, points, filled):
+    if filled == True:
+      t.begin_fill()
+    angle = 360 / points
+    for _ in range(0, points):
+      t.forward(size)
+      t.left(180 - angle)
+      t.forward(size)
+      t.right(180 - (angle * 2))
+    if filled == True:
+      t.end_fill()
+
+t.color(0, 0, 0)
+draw_star(80, 70, True)
+
+print("Help")
